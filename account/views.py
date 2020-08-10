7daa7c4ed7ab4c2270f7d78b78ca36cc0fc35f7d
Araz7899 <arazalinejad@outlook.com>
2020-08-10 10:32:35 +0430

    ja62

--- account/views.py
+++ account/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin, SuperUserAccessMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
+from .models import User
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -29,3 +32,27 @@ class ArticleDelete(SuperUserAccessMixin, DeleteView):
 	model 		  = Article
 	success_url   = reverse_lazy('account:home')
 	template_name = "registration/article_confirm_delete.html"
+
+class Profile(LoginRequiredMixin, UpdateView):
+	model         = User
+	template_name = "registration/profile.html"
+	form_class	  = ProfileForm	
+	success_url	  = reverse_lazy("account:profile")
+
+	def get_object(self):
+		return User.objects.get(pk = self.request.user.pk)
+
+	def get_form_kwargs(self):
+		kwargs = super(Profile, self).get_form_kwargs()
+		kwargs.update({'user': self.request.user})
+		return kwargs
+
+class Login(LoginView):
+	def get_success_url(self):
+		user = self.request.user
+
+		if user.is_superuser or user.is_author:
+			return reverse_lazy("account:home")
+		
+		else:
+			return reverse_lazy("account:profile")
--- blog/models.py
+++ blog/models.py
@@ -41,16 +41,17 @@ class Article(models.Model):
         ('I','در‌حال بررسی'),
         ('B','رد‌شده'),
     )
-    author      = models.ForeignKey(User , null=True, on_delete=models.SET_NULL, related_name='articles', verbose_name="نویسنده")
-    title       = models.CharField(max_length=30,verbose_name="عنوان مقاله")
-    slug        = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
-    category    = models.ManyToManyField(Category, verbose_name="دسته‌بندی", related_name="articles")
-    description = models.TextField(verbose_name="محتوای مقاله")
-    thumbnail   = models.ImageField(upload_to="images",verbose_name="تصویر مقاله")
-    publish     = models.DateTimeField(default=timezone.now, verbose_name="تاریخ انتشار")
-    created     = models.DateTimeField(auto_now_add=True)
-    updated     = models.DateTimeField(auto_now=True)
-    status      = models.CharField(max_length=1, choices=STATUS_CHOICE, verbose_name="وضعیت")
+    author       = models.ForeignKey(User , null=True, on_delete=models.SET_NULL, related_name='articles', verbose_name="نویسنده")
+    title        = models.CharField(max_length=30,verbose_name="عنوان مقاله")
+    slug         = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    category     = models.ManyToManyField(Category, verbose_name="دسته‌بندی", related_name="articles")
+    description  = models.TextField(verbose_name="محتوای مقاله")
+    thumbnail    = models.ImageField(upload_to="images",verbose_name="تصویر مقاله")
+    publish      = models.DateTimeField(default=timezone.now, verbose_name="تاریخ انتشار")
+    created      = models.DateTimeField(auto_now_add=True)
+    updated      = models.DateTimeField(auto_now=True)
+    is_special   = models.BooleanField(default=False, verbose_name="مقاله ویژه")
+    status       = models.CharField(max_length=1, choices=STATUS_CHOICE, verbose_name="وضعیت")
 
     class Meta:
         verbose_name = "مقاله"
